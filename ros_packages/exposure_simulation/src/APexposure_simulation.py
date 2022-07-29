f845b303817c4c218c53f443caa32e7cb5531f44
ivddorrka <ivddorrka@gmail.com>
2022-07-29 17:37:41 +0200

    test_try

--- ros_packages/exposure_simulation/src/APexposure_simulation.py
+++ ros_packages/exposure_simulation/src/APexposure_simulation.py
@@ -5,104 +5,81 @@ from apriltag_ros.msg import AprilTagDetectionArray
 import dynamic_reconfigure.client
 from std_msgs.msg import String
 from threading import Thread, Lock
+import numpy as np
+from cv_bridge import CvBridge
+from sensor_msgs.msg import Image
+from cv_bridge import CvBridge, CvBridgeError
 
-class TagDetectorExposureSimulation():
+# if 0 -> minimum exposure will be found
+# otherwise max will be found
+global flag_min_or_max
+
+# callback for colo image subscriber
+def ImageCallback(image_message):
+    rospy.loginfo("Made an rgb image callback")
+
     
-    def __init__(self, exposure):
-        self.result = False
-        self.exposure = exposure
-        self.msg = None
-        self.client = dynamic_reconfigure.client.Client("/camera/rgb_camera", timeout=30, config_callback=self.callback)
-        self.client.update_configuration({"exposure":exposure})
-        if exposure == 0:
-            rospy.sleep(1)
+# called when exposure changes
+def ReconfigureCallback(imsg):
+    rospy.loginfo("New exposure set")
+
+def tagDetector_callback(msg):
+    #if msg.detections!=[]:
+    rospy.loginfo(msg)
+    #rospy.signal_shutdown(msg)
+
+
+#main logic in tagdetect
+def TagDetectedCallback(tag_detections_message, args):
+    
+    flag_min_or_max = args
+
+    publisher = rospy.Publisher('exposure_info', String, queue_size=10)
+
+    client = dynamic_reconfigure.client.Client("/camera/rgb_camera", timeout=30, config_callback=ReconfigureCallback)
+
+
+    current_exposure = 0
+    if flag_min_or_max==0:
+        rospy.loginfo(f"current exposure = {current_exposure}")
+        rospy.loginfo(f"TAG DETECTIONS = {tag_detections_message.detections}")
+        client.update_configuration({"exposure":current_exposure})
+        rospy.sleep(1.5)
+
+        if tag_detections_message.detections != []:
+            publisher.publish(f"the seeking exposure = {current_exposure}")
+            rospy.signal_shutdown("Found needed exposure")
         else:
-            rospy.sleep(0.05)
-        self.subscriber = rospy.Subscriber("/tag_detections", AprilTagDetectionArray, self.get_tag)
-        
-    def callback(self, config):
-        rospy.loginfo(f"Called back exposure = {self.exposure}")
-
-    def get_tag(self, msg):
-        if msg.detections != []:
-            self.msg = msg
-            rospy.set_param("detected_flag", 1)
-        else:
-            rospy.set_param("detected_flag", 0)
-        self.subscriber.unregister()
+            rospy.loginfo(tag_detections_message)
+            current_exposure += 1
+            publisher.publish(f"current exposure = {current_exposure}")
 
 
+# the function to find the smallest or the largest exposure
+# depending on the min_or_max_flag 
+# if it's 0 - then the smallest will, otherwise the largest
+def main_so_far(setter_to_a_flag):
+    flag_min_or_max = setter_to_a_flag
+    rospy.init_node("dynamic_exposure")
+    current_exposure = 0
+    
+    client = dynamic_reconfigure.client.Client("/camera/rgb_camera", timeout = 30, config_callback=ReconfigureCallback)
+    rospy.sleep(1)
 
-def callback(config):
-    rospy.loginfo("exposure callback")
+    while not rospy.is_shutdown():
+        client.update_configuration({"exposure":current_exposure})
+        #image_subscriber = rospy.Subscriber("camera/realsense2_camera/color/image_raw", Image, ImageCallback)
+        #rospy.sleep(1.5) #to give time to subscribe and execute ImageCallback function
+    
+        tag_detections_subscriber = rospy.Subscriber("/tag_detections", AprilTagDetectionArray, tagDetector_callback)
+        rospy.sleep(1)
+        rospy.loginfo("subscribed")
+        current_exposure += 1
+    #rospy.spin()   
 
 
 if __name__ == "__main__":
-    rospy.init_node("dynamic_exposure")
-    i = 0
-    r = rospy.Rate(1000)
-    i_min = 0
-    i_max = 10000
-    
-    rospy.set_param("smallest_exposure", -1)
-    rospy.set_param("largest_exposure", -1)
-    pub = rospy.Publisher('results_top', String, queue_size=10)
-    change_exposure_by = 400
-    
-    while not rospy.is_shutdown():
-        rospy.set_param("detected_flag", 0)     
-        cl = TagDetectorExposureSimulation(i)
+    setter_to_a_flag = 0 # if 0 - then the minimum exposure will be found
+    # if that is set to 1 -> then the maximum exposure will be found
+    main_so_far(0)
 
-        detected_flag = rospy.get_param("detected_flag")
-        largest_expo = rospy.get_param("largest_exposure")
-        smallest_expo = rospy.get_param("smallest_exposure")
-
-        if detected_flag == 0:
-            
-            if largest_expo!=-1 and largest_expo>i and change_exposure_by>1 and i>100:
-                i_max = i
-                rospy.set_param("largest_exposure", i_max)
-                change_exposure_by = int(change_exposure_by / 2)
-                i -= change_exposure_by
-
-            elif smallest_expo==-1 and largest_expo==-1:
-                i +=1
-            
-            #elif largest_expo!=-1 and smallest_expo!=-1:
-                #pub.publish(f"min exposure = {i_min}, max exposure = {i_max}, i_min = {i_min}, i_max = {i_max}")
-                #rospy.loginfo("Found min and max")
-                #rospy.signal_shutdown("The end")
-
-            
-        else:
-
-            if i == 10000:
-                i_max = i
-                pub.publish(f"min exposure = {i_min}, max exposure = {i_max}, i_min = {i_min}, i_max = {i_max}")
-                rospy.loginfo("Found min and max")
-                rospy.signal_shutdown("The end")
-
-            if smallest_expo==-1:
-                i_min = i
-                rospy.set_param("smallest_exposure", i)
-                i += change_exposure_by
-
-            elif smallest_expo!= -1 and change_exposure_by>1 and i<10000:
-                i_max = i
-                rospy.set_param("largest_exposure", i)
-                
-                i += change_exposure_by
-                if i > 10000:
-                    i = 10000
-
-             
-
-
-            #elif smallest_expo !=-1 and i<=i_max and largest_expo!=-1:
-
-                #rospy.loginfo("minimum detecting exposure = {i_min}, maximum = {i_max}")
-                #rospy.signal_shutdown("Found all")
-
-        pub.publish(f"min exposure = {i_min}, max exposure = {i_max}, i_min = {i_min}, i_max = {i_max}")
-        
-        r.sleep()
